6048755c0fb684375748c32c821edbac3fdee921
felixNyalenda <felixasejunior@gmail.com>
2023-05-18 15:57:46 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+  end
+
+  def match arr
+    arr.select {|anagram| anagram.downcase.chars.sort == word.downcase.chars.sort}
+  end
+end
+
+array = %w[enlists google inlets banana]
+
+listen = Anagram.new('listen')
+puts listen.match array
